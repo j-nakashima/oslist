03fd292f9fb419bb161972f832ff7ce05688b381
j-nakashima <13532962+j-nakashima@users.noreply.github.com>
2021-11-24 11:04:30 +0900

    :tada: First implement.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,122 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1>業務委託者検索</h1>
+    <form @submit.prevent="search">
+      <div class="fullname">
+        検索する氏名（姓名） : <input type="text" v-model="fullname" placeholder="例）大分 太郎" required>
+        <input type="submit" value="search" class="btn">
+        <p class="s-mess">姓と名の間に全角スペースを入れてください。完全一致で検索します。</p>
+      </div>
+    </form>
+    <div v-if="message" class="message">{{ message }}</div>
+    <div v-if="loading" class="container" id="loading">
+      <img src="./assets/loading-19.gif" alt="loading">
+      しばらくお待ちください
+    </div>
+    <div class="container" v-html="html"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      first_name : '',
+      last_name : '',
+      fullname : '',
+      token: '',
+      html: '',
+      loading: false,
+      message: '',
+      base_url: ''
+    }
+  },
+  watch:{
+    token: {
+      handler: function(){
+        sessionStorage.setItem('formifytoken', this.token);
+      }
+    }
+  },
+  methods:{
+    initValue: function(){
+      this.first_name = '';
+      this.last_name = '';
+      this.html = '';
+      this.message = '';
+    },
+    checkToken: function(mess){
+      this.token = sessionStorage.getItem('formifytoken');
+      if(this.token){
+        return;
+      }
+      if(mess){
+        alert("セッションが終了しました");
+      }
+      this.getToken();
+    },
+    getToken: function(){
+      var username = process.env.VUE_APP_TOKEN_ID;
+      var password = process.env.VUE_APP_TOKEN_PASSWD;
+      var requestOptions = {
+        method: "POST",
+        headers: { "Content-Type":"application/json"},
+        body: JSON.stringify({auth: {signin_name: username, password: password}})
+      };
+      fetch(this.base_url + '/user_token', requestOptions)
+        .then(response => response.json())
+        .then(data => {this.token = data.jwt;})
+    },
+    search: function(){
+      this.checkToken(true);
+      this.fullname.trim();
+      var _name = this.fullname.split('　');
+      this.last_name = _name[0];
+      _name.shift();
+      this.first_name = _name.join('　');
+      if(!this.last_name){
+         this.message = '姓を入力してください';
+         this.first_name = '';
+         return;
+      }
+      if(!this.first_name){
+         this.message = '名を入力してください';
+         this.last_name = '';
+         return;
+      }
+      var params = {status: 'approved', category_id: 3, item_id_8 : this.first_name, item_id_7 : this.last_name};
+      var qparam = new URLSearchParams(params);
+      var requestOptions = {
+         method: "GET",
+         headers: { "Authorization": "Bearer " + this.token,
+             "Content-Type" : "application/html" }
+      };
+      this.loading=true;
+      this.initValue();
+      fetch(this.base_url + '/full_list/?' + qparam, requestOptions)
+          .then(response => response.text())
+          .then(data => this.html = data)
+          .then(console.log(this.html))
+          .catch(error => {
+              console.log(error);
+              this.message = '検索に失敗しました';
+          })
+          .finally(() => this.loading = false)
+    }
+  },
+  created(){
+    this.base_url = process.env.VUE_APP_BASE_URL;
+    this.checkToken(false);
+    this.initValue();
+    this.token = sessionStorage.getItem('formifytoken');
   }
 }
 </script>
 
 <style>
+@import "./assets/styles.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
